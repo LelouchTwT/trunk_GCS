377161ed1b9c9594f84acd20f5edbbb5ece18619
João Pedro Antoniazi Tonello <jpantoniazitonello@gmail.com>
2023-09-01 18:55:29 -0300

    Construtor e getters

--- src/CD.java
+++ src/CD.java
@@ -5,4 +5,44 @@ public class CD {
     String nome;
     int data_aquisicao;
     ArrayList<String> autores = new ArrayList<String>();
+    //Especificas
+    String genero;
+    ArrayList<String> musicas = new ArrayList<String>();
+
+    public CD(int id, String nome, int data_aquisicao, ArrayList<String> autores, String genero, ArrayList<String> musicas) {
+        this.id = id;
+        this.nome = nome;
+        this.data_aquisicao = data_aquisicao;
+        this.autores = autores;
+        this.genero = genero;
+        this.musicas = musicas;
+    }
+
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getData_aquisicao() {
+        return this.data_aquisicao;
+    }
+
+    public ArrayList<String> getAutores() {
+        return this.autores;
+    }
+
+    public String getGenero() {
+        return this.genero;
+    }
+
+    public ArrayList<String> getMusicas() {
+        return this.musicas;
+    }
+    
 }
+
+    
