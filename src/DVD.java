0d1fd3ffff2de73a06608389581b9747fbd2dd1b
João Pedro Antoniazi Tonello <jpantoniazitonello@gmail.com>
2023-09-08 17:55:35 -0300

    Item dvd

--- src/DVD.java
+++ src/DVD.java
@@ -1,3 +1,43 @@
+import java.util.ArrayList;
+
 public class DVD {
-    
+    int id;
+    String nome;
+    int dataAquisicao;
+    ArrayList<String> autores = new ArrayList<String>();
+
+    int tipo;
+    String descricao;
+
+    public DVD(int id, String nome, int dataAquisicao, ArrayList<String> autores, int tipo, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.dataAquisicao = dataAquisicao;
+        this.autores = autores;
+        this.tipo = tipo;
+        this.descricao = descricao;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getDataAquisicao() {
+        return this.dataAquisicao;
+    }
+
+    public ArrayList<String> getAutores() {
+        return this.autores;
+    }
+
+    public int getTipo() {
+        return this.tipo;
+    }
+    public String getDescricao() {
+        return this.descricao;
+    }
 }
